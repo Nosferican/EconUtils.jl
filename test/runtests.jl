ec067dc6b42681067a850a9a77fd60e5fd505832
Nosferican <naoyabpr@gmail.com>
2017-11-19 13:01:45 -0600

    Verify clones

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,18 @@
+Pkg.rm("DataFrames")
+Pkg.rm("DataFrames")
+Pkg.rm("StatsBase")
+Pkg.rm("StatsBase")
+Pkg.rm("StatsModels")
+Pkg.rm("StatsModels")
+Pkg.clone("https://github.com/JuliaEconometrics/DataFrames.jl")
+Pkg.clone("https://github.com/JuliaEconometrics/StatsBase.jl")
+Pkg.clone("https://github.com/JuliaEconometrics/StatsModels.jl")
+
 using EconUtils
 using Test
+using DataFrames
+
+df = DataFrames(x = 1:5)
 
 # write your own tests here
-@test 1 == 2
+@test df == DataFrames(x = 1:5)
